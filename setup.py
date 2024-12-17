76449b91654142d75e988990f3e4a0ac78c92cfb
Trevor22 <kasasatrevor25@gmail.com>
2024-12-17 14:39:20 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Kasasa Livingstone Trevor",
+    author_email="kasasatrevor25@gmail.com",
+    packages=find_packages()
+)
